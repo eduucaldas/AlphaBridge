95c1eac08c89f23ed99e66384ba41d068ef90615
Eduardo Caldas <eduucaldas@gmail.com>
2017-12-27 18:43:20 +0100

    implement parse for command rs

--- auxiliary_functions/parseHeading.py
+++ auxiliary_functions/parseHeading.py
@@ -5,7 +5,17 @@ the .lin used for examples were 46370 if you want to check it:
 http://www.bridgebase.com/tools/handviewer.html?bbo=y&linurl=http://www.bridgebase.com/tools/vugraph_linfetch.php?id=46370
 """
 
+errorLog = {'dealer':0, 'hands': 0,'lead': 0, 'bid':0, 'leader':0, 'bidding': 0, 'heading': 0}
+#we can perhaps implement it to show in which file the error occurred occurred
 
+#for readability and making it easier to change after :)
+error = {'dealer':-1, 'hands': None, 'lead': None, 'bid':None, 'leader':-1, 'bidding': None, 'contract': None,'declarer': None, 'tricks': -1}
+
+#from observation order of hands is always: SWNE
+PLAYERS = {0:'S', 1:'W', 2:'N', 3:'E'}
+OUT_OF_RANGE = 'U'# dirty trick: U for no out of range exception in SUITS
+SUITS = ['S', 'H', 'D', 'C', OUT_OF_RANGE] 
+SUITSBID = ['N'] + SUITS[:4]
 
 def parse_vg(raw_input):
 	#input:
@@ -23,14 +33,68 @@ def parse_vg(raw_input):
 ################################################
 def parse_result(code):
 	#input:(gotta finish this)
-    #    Result, in the format:(gotta finish this)
-    #	 ABC(X)DE, AB: contract, 
-    #	 Examples:(gotta finish this)
-    #	 1NW=,3NW-2,5CSx-2,PASS
-    #comments:
-    #	 Still gotta find out how redouble is encoded
-    #output:
-    #    (gotta finish this)
+	#    Result, in the format:(gotta finish this)
+	#	 ABC(X)DE, AB: contract, 
+	#	 Examples:(gotta finish this)
+	#	 1NW=,3NW-2,5CSx-2,PASS
+	#comments:
+	#	 Still gotta find out how redouble is encoded
+	#	 Problem with redouble!, verify if it is really xx in test!
+	#output:
+	#   (isPass, contract, declarer, double, tricks) 
+	#	(isPass as bool, contract as "[level][trump]", declarer as char, double as int (almost bool but, if redouble then 2), number of tricks as int)
+
+	isPass = False
+	contract = error['contract']
+	declarer = error['declarer']
+	double = 0
+	tricks = error['tricks']
+
+	if(len(code) < 4):
+		errorLog['heading']+=1
+	elif(code == "PASS"):
+		isPass = True
+	else:
+		pt = 0#this is the pointer in the array
+		if(((int(code[pt])-1) in range(7)) and (code[pt + 1] in SUITSBID)):
+			contract = code[pt:pt+2]
+		else:
+			errorLog['heading']+=1
+			return (isPass, contract, declarer, double, tricks)
+		pt+=2
+
+		if(code[pt] in PLAYERS.values()):
+			declarer = code[pt]
+		else:
+			errorLog['heading']+=1
+			return (isPass, contract, declarer, double, tricks)
+
+		double = 0
+		pt += 1#beginning of the tricks parsing
+		if(code[pt] == 'x' and len(code) > pt + 1):
+			pt += 1
+			double = 1
+		if(code[pt] == 'x' and len(code) > pt + 1):#what is the code for redouble, i think it is 'xx'
+			pt += 1
+			double = 2
+
+		tricks = 6 + int(code[0])
+		
+		if(code[pt] == '+'):
+			if((len(code) == pt + 2) and (tricks + int(code[pt+1]) in range(14))):
+				tricks += int(code[pt+1])
+			else:
+				errorLog['heading']+=1
+		elif(code[pt] == '-'):
+			if((len(code) == pt + 2) and (tricks - int(code[pt+1]) in range(14))):
+				tricks -= int(code[pt+1])
+			else:		
+				errorLog['heading']+=1
+		elif(code[pt] != '='):
+			errorLog['heading']+=1
+
+	return (isPass, contract, declarer, double, tricks)
+
 
 
 def parse_rs(raw_input):
@@ -44,10 +108,10 @@ def parse_rs(raw_input):
     #output:
     #    all information that was in the string
 
-    return [parse_result(res) for res in raw_input.split(',')]
+    return [parse_result(res) for res in raw_input.split(',') if res != ""]
 ################################################
 
-def parse_pn(raw_input):
+#def parse_pn(raw_input):
 	#input:
     #    String in the format:
     #	 "[player_1],...,[player_n]", with n from 8 to 12(according to zigfrid)
@@ -57,4 +121,13 @@ def parse_pn(raw_input):
     #	 
     #output:
     #    list of players in the right order(see HowToReadHeading)
-    
+
+def test_parse_rs():
+    raw_in = ",,,,,,,,,,,,,,,,,,,PASS,4HE=,3NW-2,2NE=,2CE+1,7NWx=,7DE=,3NS=,3NN+1,2SN+1,2SN+1,6HN+1,6HN=,2CS+1,3NS+1,3NW-1,3NW="
+    treated = parse_rs(raw_in)
+    inp = [res for res in raw_in.split(',') if res != ""]
+    print(zip(inp, treated))
+
+if __name__ == "__main__":
+	test_parse_rs()
+	
