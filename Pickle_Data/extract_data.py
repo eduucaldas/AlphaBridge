43687a92f54882d41a1bbd2f5c013f50f587292c
Eduardo Caldas <eduucaldas@gmail.com>
2018-02-05 15:25:09 +0100

    more linting

--- Pickle_Data/extract_data.py
+++ Pickle_Data/extract_data.py
@@ -8,10 +8,12 @@ from __future__ import print_function
 #
 # First you should unrar the file All.rar and it has 1 Gb in size when uncompressed!
 #
-# Put the file encode_and_parsing.py in the same folder that this script. It's in the folder auxiliary_functions of the repository.
-# It has Dudu's functions to encode and decode the bidding. Check it out to see our definitions in more detail.
+# Put the file encode_and_parsing.py in the same folder that this script. It's in the folder auxiliary_functions of
+# the repository. It has Dudu's functions to encode and decode the bidding. Check it out to see our definitions in
+# more detail.
 #
-# After that you are good to go. You can import this file inside some other code and call load_file(#insert directory of All.bin#) or search_biddings(DATA, #list of biddings to search#).
+# After that you are good to go. You can import this file inside some other code and call load_file(#insert directory
+#  of All.bin#) or search_biddings(DATA, #list of biddings to search#).
 #
 # If it's easier, you can put your code here as well.
 #
@@ -27,15 +29,15 @@ import auxiliary_functions.encode_and_parsing as eap
 
 class BridgeDeal:
     def __init__(self, bbo_file, tournament, players, hands, bidding, leader, dealer, lead, vuln):
-        self.bbo_file = bbo_file
+        self.bbo_file = bbo_file  # number specifying the id of the .lin given by bbo
         self.tournament = tournament  # not done yet!
         self.players = players  # not done yet!
-        self.hands = hands
-        self.bidding = bidding
-        self.leader = leader
-        self.dealer = dealer
-        self.lead = lead
-        self.vuln = vuln
+        self.hands = hands  # 4 hands, each as a 4x13 matrix with 1 in the entries in which the card is present
+        self.bidding = bidding  # the bidding sequence as encoded in encode_and_parsing
+        self.leader = leader  # i.e. the person that puts the first card in the game, as a number(0-3) TODO check Ian
+        self.dealer = dealer  # i.e. the person that puts the first bids, as a number (0-3) TODO check Ian
+        self.lead = lead  # i.e. the first card to be played
+        self.vuln = vuln  # i.e. vulnerability TODO check Ian
 
     def __eq__(self, other):
         return self.bbo_file == other.bbo_file and self.tournament == other.tournament and self.players == other.players and self.bidding == other.bidding and (
@@ -45,9 +47,9 @@ class BridgeDeal:
 # save DATA to file
 
 
-def save_file(path, DATA):
+def save_file(path, data):
     f = open(path, "wb")
-    pickle.dump(DATA, f)
+    pickle.dump(data, f)
     f.close()
 
 
@@ -56,9 +58,9 @@ def save_file(path, DATA):
 
 def load_file(path):
     f = open(path, "rb")
-    DATA = pickle.load(f)
+    data = pickle.load(f)
     f.close()
-    return DATA
+    return data
 
 
 # We are going to get all matches with a particular bidding. For example, any of the following list:
@@ -69,11 +71,11 @@ def load_file(path):
 def find_biddings(DATA, biddings):
     DATA_match = []
 
-    # Dudu has created a coded version of the bidding, it occupies less space and it'll be easier to run ML and DL later.
-    # We need to encode the biddingit
+    # Dudu has created a coded version of the bidding, it occupies less space and it'll be easier to run ML and DL later
+    # We need to encode the bidding
     bidding_encoded = []
     for bidding in biddings:
-        bidding_encoded.append(eap.encodeRaw_Bidding(bidding))
+        bidding_encoded.append(eap.encode_raw_Bidding(bidding))
 
     for deal in DATA:
         for bidding in bidding_encoded:
@@ -86,46 +88,47 @@ def find_biddings(DATA, biddings):
 def extract_to_temp(in_pickle, biddings):
     start = time.time()
 
-    DATA = []  # list of BridgeDeal objects
-
     print("Reading data from disk...")
     # let's load the entire database (4 Gb in memory)
     # in my case the file is in the same folder.
-    DATA = load_file(in_pickle)
+    data = load_file(in_pickle)
 
     end_load = time.time()
     print(" ...Done. Time to read data from disk: {:,.2f} seconds".format(end_load - start))  # time to retrieve DATA
 
-    # as an example, lets search for:
-    biddings = [['1N', 'p', 'p', 'p']]
-
     print("Searching for biddings:")
     print(biddings)
 
-    DATA_match = find_biddings(DATA, biddings)
+    data_match = find_biddings(data, biddings)
 
     end_search = time.time()
     print(" ...Done. Searched in: {:,.2f} seconds".format(end_search - end_load))  # time to retrieve DATA
-    print("Number of games found: " + str(len(DATA_match)))
+    print("Number of games found: " + str(len(data_match)))
 
-    return DATA_match
+    return data_match
     # Do whatever... ML, DL...
 
 
 # now, if you want to save to file
 
 
-def extract_to_pickle(in_pickle, outdir, biddings):
+def extract_to_pickle(in_pickle, out_dir, biddings):
     outfile = "Search.bin"
-    if (outdir != ""):
-        outfile = outdir + "/" + outfile
+    if out_dir != "":
+        outfile = out_dir + "/" + outfile
     data = extract_to_temp(in_pickle, biddings)
-    file = open(outfile, 'wb')
-
-    pickle.dump(data, file)
+    save_file(outfile, data)
 
 
-# This is probably how you'll use it, ../1NTPPP comes to the parent directory and enters in the 1NTPPP folder
+def test():
+    test_data = extract_to_temp("All.bin", [['1N', 'p', 'p', 'p']])
+    for i in range(100):
+        print(eap.decodeBidding(test_data[i].bidding))
+
+
 if __name__ == '__main__':
-    extract_to_pickle("All.bin", "../1NTPPP",
-                      [['1N', 'p', 'p', 'p'], ['p', '1N', 'p', 'p'], ['p', 'p', '1N', 'p'], ['p', 'p', 'p', '1N']])
+    test()
+    # This is probably how you'll use it, ../1NTPPP comes to the parent directory and enters in the 1NTPPP folder
+
+    # extract_to_pickle("All.bin", "../1NTPPP",
+    #                  [['1N', 'p', 'p', 'p'], ['p', '1N', 'p', 'p', 'p'], ['p', 'p', '1N', 'p', 'p', 'p'], ['p', 'p', 'p', '1N', 'p', 'p', 'p']])
--- auxiliary_functions/encode_and_parsing.py
+++ auxiliary_functions/encode_and_parsing.py
@@ -6,18 +6,19 @@ Created on Thu Oct  5 10:18:39 2017
 """
 import numpy as np
 
-#list of all commands in the lin files
-commands = {'md':0, 'sv':1, 'pc':2, 'mb':3, 'ob':4, 'qx':5, 'mc':6, 'pg':7, 'nt':8, 'vg':9, 'rs': 10, 'pn': 11, 'an':12, 'st': 13}
+# list of all commands in the lin files
+commands = {'md': 0, 'sv': 1, 'pc': 2, 'mb': 3, 'ob': 4, 'qx': 5, 'mc': 6, 'pg': 7, 'nt': 8, 'vg': 9, 'rs': 10,
+            'pn': 11, 'an': 12, 'st': 13}
 
-#from observation order of hands is always: SWNE
-PLAYERS = {0:'S', 1:'W', 2:'N', 3:'E'}
-#from observation order of suits is always: SHDC
-SUITMAP = {'S':0, 'H':1, 'D':2, 'C':3}
+# from observation order of hands is always: SWNE
+PLAYERS = {0: 'S', 1: 'W', 2: 'N', 3: 'E'}
+# from observation order of suits is always: SHDC
+SUITMAP = {'S': 0, 'H': 1, 'D': 2, 'C': 3}
 
-OUT_OF_RANGE = 'U'# dirty trick: U for no out of range exception in SUITS
+OUT_OF_RANGE = 'U'  # dirty trick: U for no out of range exception in SUITS
 SUITS = ['S', 'H', 'D', 'C', OUT_OF_RANGE]
 # this is how the cards are described in the .lin
-CARDMAP = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':11, 'Q':12, 'K':13, 'A':14}
+CARDMAP = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
 
 # SUITSBID = ['N'] + SUITS[:4]
 # BIDS = ['P','R','D'] + [str(i + 1) + s for i in range(7) for s in SUITSBID]
@@ -39,62 +40,62 @@ BIDSMAP = {'P': 0, 'R': 1, 'D': 2,
            '6N': 28, '6S': 29, '6H': 30, '6D': 31, '6C': 32,
            '7N': 33, '7S': 34, '7H': 35, '7D': 36, '7C': 37}
 # Count where the error occurred, e.g. if it was in dealer
-errorLog = {'dealer':0, 'hands': 0,'lead': 0, 'bid':0, 'leader':0, 'bidding': 0}
+errorLog = {'dealer': 0, 'hands': 0, 'lead': 0, 'bid': 0, 'leader': 0, 'bidding': 0}
 # we can perhaps implement it to show in which file the error occurred occurred
 
 # for readability and making it easier to change after :)
-error = {'dealer':-1, 'hands': None, 'lead': None, 'bid':None, 'leader':-1, 'bidding': None}
+error = {'dealer': -1, 'hands': None, 'lead': None, 'bid': None, 'leader': -1, 'bidding': None}
 
 
-
-#resume of meaning for lin files
-#qx -> begin game
-#md -> show hands
-#sv -> start bidding
-#mb -> make bid
-#pc -> next card
-#pg -> next line, followed by ||
-#mc -> finish game
+# resume of meaning for lin files
+# qx -> begin game
+# md -> show hands
+# sv -> start bidding
+# mb -> make bid
+# pc -> next card
+# pg -> next line, followed by ||
+# mc -> finish game
 
 
 def pretreatBidding(raw_bidding):
-    #input:
+    # input:
     #    list of raw_bid strings
-    #comments:
+    # comments:
     #    bids should be something in BIDS, not case sensitive and maybe with a '!' at the end, we fiz all this problems
-    #output:
+    # output:
     #    list of bid strings, all capitalized and with no '!'
     bidding = []
     for bid in raw_bidding:
-        bid = bid.upper()#fix caseSensitive
-        bid = bid.replace("!", "")#fix '!'
-        if(isValidBid(bid)):#fix: in BIDS
+        bid = bid.upper()  # fix caseSensitive
+        bid = bid.replace("!", "")  # fix '!'
+        if (isValidBid(bid)):  # fix: in BIDS
             bidding.append(bid)
         else:
-            errorLog['bidding']+=1
+            errorLog['bidding'] += 1
             return error['bidding']
-
 
     return bidding
 
+
 def encodeBid(bid):
-    #input:
+    # input:
     #    bid as string.
-    #comments:
+    # comments:
     #    verifies if valid bid, ie whether its on our BIDS
-    #output:
+    # output:
     #    puts in the code according to BIDSMAP
     code = BIDSMAP.get(bid, error['bid'])
-    if(code == error['bid']):
-        errorLog['bid']+=1
+    if (code == error['bid']):
+        errorLog['bid'] += 1
     return code
 
+
 def decodeBid(code):
-    #input:
+    # input:
     #    bid coded, as an integer [0-37]
-    #comments:
+    # comments:
     #    Verifies if code is in our BIDS, warning message appears
-    #output:
+    # output:
     #    bid as a human readable bid
     try:
         return BIDS[code]
@@ -102,71 +103,77 @@ def decodeBid(code):
         print('your code was weird')
         return error['code']
 
+
 def encodeBidding(bidding):
-    #input:
+    # input:
     #    pretreated bidding as a list of strings
-    #comments:
+    # comments:
     #
-    #output:
+    # output:
     #    code as list of codes coming from each bid
-    if(bidding == error['bidding']):
+    if (bidding == error['bidding']):
         return error['bidding']
     errorStart = errorLog['bid']
     code = [encodeBid(bidding[i]) for i in range(len(bidding))]
-    if(errorLog['bid'] - errorStart == 0):
+    if (errorLog['bid'] - errorStart == 0):
         return code
     else:
-        errorLog['bidding']+=1
+        errorLog['bidding'] += 1
         return error['bidding']
 
+
 def isValidBid(bid):
-    #input:
+    # input:
     #    bid as string
-    #comments:
+    # comments:
     #    Attention, this verifies only if the bid has the right size, with '!' or not
-    #output:
+    # output:
     #    boolean: is it in the proper format
     return (bid in BIDS)
+
+
 ###################Main Functions##########################
-def encodeRaw_Bidding(raw_bidding):
-    #input:
-    #    list of raw_bidding, just as they came out from @Ian
-    #comments:
+def encode_raw_Bidding(raw_bidding):
+    # input:
+    #    list of raw_bidding, just as they came out from @Ian, like this: ['p', '1D', '1S', '2C', '2S', '3C', 'p', '4C', 'p', '5C', 'p', 'p', 'p']
+    # comments:
     #    Look at other functions, has error handling
-    #output:
+    # output:
     #    code following BIDSMAP
     bidding = pretreatBidding(raw_bidding)
     return encodeBidding(bidding)
 
 
 def decodeBidding(code):
-    #input:
+    # input:
     #    code fo the bidding
-    #comments:
+    # comments:
     #    decodeBid has some python error handling :), if there`s a problem it`ll print message
-    #output:
+    # output:
     #    bidding as list of strings
-    if(code == None):
+    if (code == None):
         return None
     else:
         return [decodeBid(c) for c in code]
+
+
 #############################################
 
 def testDudu(raw_data):
-    #input:
+    # input:
     #    format: "1S|mb|p|mb|2D|mb|p|mb|3S!|mb|p|mb|4H|mb|d|mb|p|mb|p|mb|r|mb|p|mb|4N|mb|p|mb|5H|mb|p|mb|5N|mb|p|mb|6S|mb|p|mb|p"
-    #comments:
+    # comments:
     #    for the input just pick any .lin and pick the string between the first and the last |mb|
-    #output:
+    # output:
     #    prints the bidding list as it was read, the code list, and the bidding decoded from the code
-    #raw_data.split("|mb|")
-    #print(raw_data)
-    code = encodeRaw_Bidding(raw_data)
+    # raw_data.split("|mb|")
+    # print(raw_data)
+    code = encode_raw_Bidding(raw_data)
     bidding = decodeBidding(code)
     print(code)
     print(bidding)
 
-#testDudu(['p', '1D', '1S', '2C', '2S', '3C', 'p', '4C', 'p', '5C', 'p', 'p', 'p'])
+# testDudu(['p', '1D', '1S', '2C', '2S', '3C', 'p', '4C', 'p', '5C', 'p', 'p', 'p'])
 
 '''
 parsingFromVector.py
@@ -183,6 +190,8 @@ This script:
 """
 Auxiliar functions#############################################################
 """
+
+
 def vectorizing(handText):
     """
     input:
@@ -195,21 +204,22 @@ def vectorizing(handText):
            HandVector = 52-binary vector, with ones when the player has the card
     """
     hand = np.zeros(52, dtype=bool)
-    end = 0   #should see if its S
-    handText = handText + OUT_OF_RANGE #adds stopping character
+    end = 0  # should see if its S
+    handText = handText + OUT_OF_RANGE  # adds stopping character
     for suit in range(4):
         begin = end + 1
-        end = handText.find(SUITS[suit+1])
-        if(end == -1 or end < begin): #then we haven`t found the next suit, which is a problem according to our format
-            errorLog['hands']+=1
+        end = handText.find(SUITS[suit + 1])
+        if (
+                end == -1 or end < begin):  # then we haven`t found the next suit, which is a problem according to our format
+            errorLog['hands'] += 1
             return error['hands']
-        for i in range(begin,end):
+        for i in range(begin, end):
             if not (handText[i] in CARDMAP):
                 errorLog['hands'] += 1
                 return error['hands']
             else:
-                hand[suit*13 + CARDMAP[handText[i]]-2] = True
-    #take out our OUT_OF_RANGE? not necessary :)
+                hand[suit * 13 + CARDMAP[handText[i]] - 2] = True
+    # take out our OUT_OF_RANGE? not necessary :)
     return hand
 
 
@@ -224,27 +234,30 @@ def vectorizing3(handsText):
     handsText = handsText.split(',')
 
     for hT in handsText:
-        if(len(hT) != 17):
+        if (len(hT) != 17):
             errorLog['hands'] += 1
             return error['hands']
 
     hands = []
     for handT in handsText:
         h = vectorizing(handT)
-        if(h == None):
+        if (h == None):
             return error['hands']
         else:
             hands.append(h)
 
     one = np.ones(len(hands[0]), dtype=bool)
-    hands.append(one ^ hands[0] ^ hands[1] ^ hands[2])#appends the missing hand
-    #may need to verify if hands[3] is valable
+    hands.append(one ^ hands[0] ^ hands[1] ^ hands[2])  # appends the missing hand
+    # may need to verify if hands[3] is valable
     return hands
+
+
 """
 ###############################################################################
 """
 
-def pick_hands(gameLine):
+
+def pick_hands(game_line):
     """
     input:
        string in format:"1SKT32HJ984DJT52C9,SAQJ86HK63DK93C75,S94HT5D864CAQJT62,S75HAQ72DAQ7CK843"
@@ -259,18 +272,18 @@ def pick_hands(gameLine):
        vector of hands:
            4 HandVectors
     """
-    gameLine = gameLine.upper()
+    game_line = game_line.upper()
     dealer = error['dealer']
     hands = error['hands']
-    begin = gameLine.find(SUITS[0]) ##beginning of hands, namely the first spades that he encounters
-    if(begin == 1):
-        if(int(gameLine[0]) < 1 or int(gameLine[0]) > 4):
+    begin = game_line.find(SUITS[0])  # beginning of hands, namely the first spades that he encounters
+    if begin == 1:
+        if int(game_line[0]) < 1 or int(game_line[0]) > 4:
             errorLog['dealer'] += 1
         else:
-            handsText = gameLine[1:]
-            dealer = int(gameLine[0]) - 1#puts dealer in the format we want
-            if(len(handsText) == 71 or len(handsText) == 53):
-                #in the case of 4 hands, reduce it to 3 and solve
+            handsText = game_line[1:]
+            dealer = int(game_line[0]) - 1  # puts dealer in the format we want
+            if len(handsText) == 71 or len(handsText) == 53:
+                # in the case of 4 hands, reduce it to 3 and solve
                 handsText = handsText[:53]
                 hands = vectorizing3(handsText)
             else:
@@ -281,9 +294,7 @@ def pick_hands(gameLine):
     return dealer, hands
 
 
-
-
-def pick_lead(handsText, lead, hands):
+def pick_lead(hands_text, lead, hands):
     """
     input:
        hands in the text format, lead as string, hands in vector format
@@ -297,21 +308,21 @@ def pick_lead(handsText, lead, hands):
         hands:
            4 HandVectors in the order they`ll be played
     """
-    handsText = handsText[1:].upper().split(',')
+    hands_text = hands_text[1:].upper().split(',')
     suitlead = lead[0].upper()
     ranklead = lead[1].upper()
-    if(suitlead in SUITMAP and ranklead in CARDMAP):
+    if (suitlead in SUITMAP and ranklead in CARDMAP):
         suitCode = SUITMAP[suitlead]
         nextSuit = SUITS[suitCode + 1]
-        leader = 3 # if we dont find the lead below the leader gotta be the 4 guy
-        for h in range(len(handsText)):
-            #print(h)
-            begin = handsText[h].find(suitlead)
-            end = handsText[h].find(nextSuit)
-            if(end == -1):
-                end = len(handsText[h])
-            if(handsText[h][begin:end].find(ranklead) != -1):
-                leader = h #here we found the guy that lead, the leader
+        leader = 3  # if we dont find the lead below the leader gotta be the 4 guy
+        for h in range(len(hands_text)):
+            # print(h)
+            begin = hands_text[h].find(suitlead)
+            end = hands_text[h].find(nextSuit)
+            if (end == -1):
+                end = len(hands_text[h])
+            if (hands_text[h][begin:end].find(ranklead) != -1):
+                leader = h  # here we found the guy that lead, the leader
                 break
         hands = np.roll(hands, leader)
     else:
@@ -320,10 +331,12 @@ def pick_lead(handsText, lead, hands):
 
     return leader, hands
 
-def isCommand(info):
-	if(info in commands):
-		return True
-	return False
+
+def is_command(info):
+    if (info in commands):
+        return True
+    return False
+
 
 """
 Dudu
